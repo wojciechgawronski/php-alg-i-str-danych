8f1bd7ddda14d562de019eed1a5b3bc1d6a0e43c
W. Gawronski <wgawronski.wg@gmail.com>
2024-05-17 09:08:01 +0200

    splQueue (Standard PHP Lib)

---
+++ queue/splQueue.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+php_sapi_name() === 'cli' ? define('EOL', PHP_EOL) : define('EOL', "<br>");
+
+
+$namesQueue = new SplQueue();
+$namesQueue->push("one");
+$namesQueue->push("two");
+$namesQueue->push("three");
+
+echo "Queue:\n";
+foreach ($namesQueue as $element) {
+    echo $element . EOL;
+}
+echo EOL;
+echo 'count: '. $namesQueue->count() . EOL;
+echo 'front element (bottom): '. $namesQueue->bottom() . EOL;
+echo 'top: '. $namesQueue->top() . EOL;
+echo 'dequeue: '. $namesQueue->dequeue() . EOL;
+
+echo EOL;
+// Emptying the queue 
+while (!$namesQueue->isEmpty()) {
+    echo "Dequeued: " . $namesQueue->dequeue() . EOL;
+}
+echo 'count: '. $namesQueue->count() . EOL;
+echo EOL;
+$namesQueue->dequeue();
