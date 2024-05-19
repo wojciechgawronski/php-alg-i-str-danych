27a3e8b55a8c8897e586c085e139674e95232b13
W. Gawronski <wgawronski.wg@gmail.com>
2024-05-19 08:04:33 +0200

    bst

--- treeBST/BSTNode.php
+++ treeBST/BSTNode.php
@@ -7,20 +7,59 @@ class BSTNode
     public int $data;
     public $left = null;
     public $right = null;
+    public $parent;
 
-    public function __construct(int $data)
+    public function __construct(int $data, BSTNode $parent = null)
     {
         $this->data = $data;
+        $this->parent = $parent;
     }
 
-    // public function min()
-    // {
-    //     $node = $this;
-    //     while ($node->left) {
-    //         $node = $node->left;
-    //     }
-    //     return $node;
-    // }
+    public function delete(): void
+    {
+        $node = $this;
+        if (!$node->left && !$node->right) {
+            if ($node->parent->left === $node)
+                $node->parent->left = null;
+            else
+                $node->parent->right = null;
+        } else if ($node->left && $node->right)
+        {
+            $successor = $node->successor();
+            $node->data = $successor->data;
+            $successor->delete();
+        }
+        else if ($node->left) {
+            if ($node->parent->left === $node) {
+                $node->parent->left = $node->left;
+                $node->left->parent = $node->parent->left;
+            } else {
+                $node->parent->right = $node->left;
+                $node->left->parent = $node->parent->right;
+            }
+            $node->left = null;
+        }
+        elseif ($node->right)
+        {
+            if ($node->parent->left === $node) {
+                $node->parent->left = $node->right;
+                $node->right->parent = $node->parent->left;
+            } else {
+                $node->parent->right = $node->right;
+                $node->right->parent = $node->parent->right;
+            }
+            $node->right = null;
+        }
+    }
+
+    public function min()
+    {
+        $node = $this;
+        while ($node->left) {
+            $node = $node->left;
+        }
+        return $node;
+    }
 
     // public function max(): int
     // {
@@ -32,15 +71,15 @@ class BSTNode
     // }
 
     // następca
-    // public function successor()
-    // {
-    //     $node = $this;
-    //     if ($node->right) {
-    //         return $node->right->min();
-    //     } else {
-    //         return null;
-    //     } 
-    // }
+    public function successor()
+    {
+        $node = $this;
+        if ($node->right) {
+            return $node->right->min();
+        } else {
+            return null;
+        } 
+    }
 
     // public function predecessor()
     // {
